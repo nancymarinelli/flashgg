b79bf5d43a243a355ffe61cd345013605408f535
Nancy <nancy.marinelli@cern.ch>
2015-08-04 15:40:34 +0200

    pytin file to make microAOD suitable to be studied with Validation/plugins/DiphotonPreselectionValidation

---
+++ MicroAOD/test/microAODForPhotonPreselection.py
@@ -0,0 +1,82 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
+
+process = cms.Process("FLASHggMicroAOD")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = 'PLS170_V7AN1::All'
+#process.GlobalTag.globaltag = 'auto:run2_mc'
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+#process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 10 )
+
+
+# RelVal 744
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring(
+#"/store/relval/CMSSW_7_4_4/RelValH130GGgluonfusion_13/MINIAODSIM/MCRUN2_74_V9_38Tbis-v1/00000/4E11D911-3909-E511-B6EF-00261894393E.root",
+#"/store/relval/CMSSW_7_4_4/RelValH130GGgluonfusion_13/MINIAODSIM/MCRUN2_74_V9_38Tbis-v1/00000/DC719B1F-3909-E511-BBF7-0025905B85D6.root"
+
+"/store/relval/CMSSW_7_4_4/RelValH130GGgluonfusion_13/MINIAODSIM/PU25ns_MCRUN2_74_V9_38Tbis-v1/00000/BC6A2219-4E09-E511-A2E1-0026189438D6.root",
+"/store/relval/CMSSW_7_4_4/RelValH130GGgluonfusion_13/MINIAODSIM/PU25ns_MCRUN2_74_V9_38Tbis-v1/00000/D85999C9-4D09-E511-AA26-0026189438D6.root"
+
+))
+
+
+process.MessageLogger.cerr.threshold = 'ERROR' # can't get suppressWarning to work: disable all warnings for now
+# process.MessageLogger.suppressWarning.extend(['SimpleMemoryCheck','MemoryCheck']) # this would have been better...
+
+# Uncomment the following if you notice you have a memory leak
+# This is a lightweight tool to digg further
+#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+#                                        ignoreTotal = cms.untracked.int32(1),
+#                                        monitorPssAndPrivate = cms.untracked.bool(True)
+#                                       )
+
+process.load("flashgg/MicroAOD/flashggMicroAODSequence_cff")
+process.load("flashgg/MicroAOD/flashggPhotons_cfi")
+process.load("flashgg/MicroAOD/flashggDiPhotons_cfi")
+
+from flashgg.MicroAOD.flashggPhotons_cfi import *
+flashggPhotons.rhoCorrection = False
+
+from flashgg.MicroAOD.flashggMicroAODOutputCommands_cff import microAODDefaultOutputCommand,microAODDebugOutputCommand
+process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string('microAOD_DiPhotonPreselectionTest.root'),
+                               outputCommands = microAODDefaultOutputCommand
+                               )
+process.out.outputCommands += microAODDebugOutputCommand # extra items for debugging, CURRENTLY REQUIRED
+
+# need to allow unscheduled processes otherwise reclustering function will fail
+# this is because of the jet clustering tool, and we have to live with it for now.
+process.options = cms.untracked.PSet(
+    allowUnscheduled = cms.untracked.bool(True)
+    )
+# import function which takes care of reclustering the jets using legacy vertex		
+from flashgg.MicroAOD.flashggJets_cfi import addFlashggPFCHSLegJets 
+# call the function, it takes care of everything else.
+addFlashggPFCHSLegJets(process)
+
+
+process.p = cms.Path(process.flashggMicroAODSequence+process.flashggDiPhotons)
+process.e = cms.EndPath(process.out)
+
+# Uncomment these lines to run the example commissioning module and send its output to root
+#process.commissioning = cms.EDAnalyzer('flashggCommissioning',
+#                                       PhotonTag=cms.untracked.InputTag('flashggPhotons'),
+#                                       DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons'),
+#                                       VertexTag=cms.untracked.InputTag('offlineSlimmedPrimaryVertices')
+#)
+#process.TFileService = cms.Service("TFileService",
+#                                   fileName = cms.string("commissioningTree.root")
+#)
+#process.p *= process.commissioning
+
+
+from flashgg.MicroAOD.MicroAODCustomize import customize
+customize(process)
